a0daeb079092c8b26943a178a209c32ed3715c9e
Sarvesh sabe <Sarveshsabe@gmail.com>
2024-06-22 17:07:31 -0500

    scene

---
+++ player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+
+
+var speed = 14.0
+var jump_speed = 4.5
+var mouse_sensitivity = 0.009
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _physics_process(delta):
+	velocity.y += - gravity * delta
+	var input = Input.get_vector("left","right","up","down")
+	var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
+	velocity.x = movement_dir.x * speed
+	velocity.z = movement_dir.z * speed
+	
+	move_and_slide()
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_speed
+func _input(event):
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
