83c51ee5f095c3b433af77484b46e9f1c38625f9
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-06-22 14:16:10 -0500

    Added english translation

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,10 +1,13 @@
-import google.generativeai as genai
-import os
+import google.generativeai as genai, os
 
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-response = model.generate_content("Write a story about a AI and magic")
+prompt_addon = """
+Give tips if the user only if the user asks for them.
+Otherwise, act as a normal street vendor who sells fruits. Make sure you reprimand the user if they talk inappropriately.
+Put an english translation at the end of the prompt. Before writing the english translation, put a semicolon to signify the change.
+"""
+response = model.generate_content("You are a spanish street vendor in a game about building confidence in speaking other languages." + prompt_addon)
 print(response.text)
-
